26328331fda113e44e4d167b319b5b1720eaf535
muskahatori <ramazan2006osh@gmail.com>
2026-08-20 23:53:01 +0600

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="logo">○ aroma</div>
+        <div className="socials">
+          <a href="#">f</a>
+          <a href="#">t</a>
+          <a href="#">ig</a>
+          <a href="#">yt</a>
+        </div>
+      </footer>
+      <div className="footer-bottom">AROMA © 2026</div>
+    </>
+  )
+}
+
+export default Footer
