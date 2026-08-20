1faf68ec30cde909f797ad069e17f7e970d5e98d
muskahatori <ramazan2006osh@gmail.com>
2026-08-20 23:52:15 +0600

    Create Header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">○ aroma</div>
+      <nav className="nav">
+        <a href="#">Кофе</a>
+        <a href="#">О нас</a>
+        <a href="#">Доставка</a>
+        <a href="#">Контакты</a>
+      </nav>
+      <div className="header-icons">
+        <span>Поиск</span>
+        <span>Войти</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
