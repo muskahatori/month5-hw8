e0e2be5678d8c330667501dabd31721a8aff452b
muskahatori <ramazan2006osh@gmail.com>
2026-08-20 23:51:59 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './Header';
+import Hero from './Hero';
+import Products from './Products';
+import Partners from './Partners';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Products />
+      <Partners />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
