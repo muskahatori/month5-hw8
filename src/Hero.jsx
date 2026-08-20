f78f6c280365e0763d5ee6946d70f506a91b125d
muskahatori <ramazan2006osh@gmail.com>
2026-08-20 23:52:27 +0600

    Create Hero.jsx

---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <h1>Свежая обжарка.<br />Каждый день</h1>
+        <p>Кофе (от араб. qahwa — вино, напиток из бобов) — тонизирующий напиток, приготавливаемый из жареных семян кофейного дерева нескольких сортов.</p>
+        <button>Каталог</button>
+      </div>
+      <div className="hero-image">
+        <img src="https://picsum.photos/seed/aroma-hero/500/400" alt="кофе" />
+      </div>
+    </section>
+  )
+}
+
+export default Hero
